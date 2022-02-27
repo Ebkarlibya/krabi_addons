1b67cb38f00571ddd2cbce9d53890838ebe0ea36
Imad Abdou <igentle.appletec@gmail.com>
2022-02-27 10:16:01 +0100

    change url to /kis?d=DocName

--- krabi_addons/www/kis/index.py
+++ krabi_addons/www/kis/index.py
@@ -4,6 +4,6 @@ import frappe
 def get_context(ctx):
     ctx.no_cache = 1
 
-    is_name = frappe.request.args['name']
+    is_name = frappe.request.args['d']
     is_orm = frappe.get_doc("Inspection Certificate", is_name)
     ctx['is_orm'] = is_orm
